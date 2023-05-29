be2953e31e533e197bd10497f90d65470aa62580
yuri <yurisousaenfer@gmail.com>
2023-05-29 17:29:51 -0300

    :cadastro,paises limitrofes: sistema completo

--- POO/exe/exe_04/Pais.py
+++ POO/exe/exe_04/Pais.py
@@ -1,6 +1,7 @@
-paises = {}
+
 
 class Pais:
+    paises = {}
     def __init__(self,codigoISO, nome,populacao,dimensao,paisesFronteira=[]):
         if isinstance(codigoISO,str) and codigoISO != "":
             self.codigoISO = codigoISO.upper()
@@ -41,13 +42,31 @@ class Pais:
     def set_paisesFronteira(self,newPaisesFronteira):
         self.paisesFronteira = newPaisesFronteira 
     
-    def compararCodigoIso(self,ISO=0):
-        for x in paises:
+    def compararCodigoIso():
+        print("Paises cadastrados:")
+        print('--------------------')
+        for x in Pais.paises:
             print(x)
-            """if self.codigoISO == ISO.upper():
-                return True
-            return False"""
+        p1 = input("nome país 1: ").capitalize()
+        p2 = input("nome país 2: ").capitalize()
+        if p1 in Pais.paises and p2 in Pais.paises:
+            if Pais.paises[p1].codigoISO == Pais.paises[p2].codigoISO:
+                print('são iguais')
+            else:
+                print('são diferentes')
+        else:
+            print('Pais não cadastrado')
 
+    def paisLimitrofe(p1,p2):
+        if p1 in Pais.paises and p2 in Pais.paises:
+            if p1.lower() in Pais.paises[p2].paisesFronteira:
+                print(Pais.paises[p2].paisesFronteira)
+            else:
+                print(Pais.paises[p1].__dict__)
+                print(Pais.paises[p2].__dict__)
+                print('não fazem fronteira')
+        else:
+            print('paises não cadastrados!')
 
 
 
@@ -59,10 +78,17 @@ def cadastrar():
         'dimensao': float(input('Dimensão: ')),
         'front': input("paises que fazem fronteiras (utilize ',' para separar): ").split(",")
     }
+    front = ''
+    for x in dados['front']:
+        if front == '':
+            front = x
+        else:
+            front += f",{x}"
+
     inst = Pais(dados['codigoISO'], dados['nome'], int(dados['populacao']), float(dados['dimensao']), list(dados['front']))
-    paises[dados['nome']] = inst  # Adiciona o país ao dicionário paises
+    Pais.paises[dados['nome']] = inst  # Adiciona o país ao dicionário paises
     with open('paises.txt', 'a') as file:
-        file.write(f"{dados['codigoISO']},{dados['nome']},{dados['populacao']},{dados['dimensao']}, {dados['front']}\n")
+        file.write(f"{dados['codigoISO']},{dados['nome']},{dados['populacao']},{dados['dimensao']}, {front}\n")
     print(f"{dados['nome']} cadastrado com sucesso!")
 
 
@@ -82,22 +108,21 @@ def menu():
     elif acao == "1":
         cadastrar()
     elif acao == "2":
+        Pais.compararCodigoIso()
+    elif acao == '3':
+        print("Paises cadastrados:")
+        print('--------------------')
+        for x in Pais.paises:
+            print(x)
         p1 = input("nome país 1: ").capitalize()
         p2 = input("nome país 2: ").capitalize()
-        if p1 in paises and p2 in paises:
-            if paises[p1].codigoISO == paises[p2].codigoISO:
-                print('são iguais')
-            else:
-                print('são diferentes')
-        else:
-            print('Pais não cadastrado')
-
+        return Pais.paisLimitrofe(p1, p2)
     # Restante do código...
 
 if __name__ == "__main__":
     with open("paises.txt", 'r') as file:
         for obj in file:
-            x = obj.split(',')
-            paises[x[1]] = Pais(x[0], x[1], int(x[2]), float(x[3]), list(x[4]))
-        print(paises)
+            x = obj.split(',',maxsplit=4)
+            Pais.paises[x[1]] = Pais(x[0], x[1], int(x[2]), float(x[3]), x[4].replace('[', '').replace(']', '').strip().split(','))
+        print(Pais.paises)
     menu()
